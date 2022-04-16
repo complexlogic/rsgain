2e2429abf11f6e476a769c0076445ef8e3431b84
complexlogic <complexlogic@users.noreply.github.com>
2022-04-15 20:26:52 -0700

    Refactored scan.py script, renamed to loudgain-scanner.py

--- scripts/loudgain-scanner.py
+++ scripts/loudgain-scanner.py
@@ -1,19 +1,29 @@
+#!/usr/bin/env python3
 import os
 import sys
 import shutil
 import glob
 import subprocess
+import platform
 
-SCAN_ARGS = {'.flac': ['-a', '-k', '-s', 'e'],
-             '.ogg': ['-a', '-k', '-s', 'e'],
-             '.mp3': ['-I3', '-S', '-L', '-a', '-k', '-s', 'e'],
-             '.m4a': ['-L', '-a', '-k', '-s', 'e'],
-             '.opus': ['-a', '-k', '-s', 'e'],
-             '.wma': ['-L', '-a', '-k', '-s', 'e'],
-             '.wav': ['-I3', '-L', '-a', '-k', '-s', 'e'],
-             '.aiff': ['-I3', '-L', '-a', '-k', '-s', 'e'],
-             '.wv': ['-S', '-a', '-k', '-s', 'e'],
-             '.ape': ['-S', '-a', '-k', '-s', 'e']}
+loudgain = "loudgain"
+if platform.system == "Windows":
+    EXECUTABLE_TITLE = loudgain + ".exe"
+else:
+    EXECUTABLE_TITLE = loudgain
+
+SCAN_ARGS = {
+    '.flac': ['-a', '-k', '-s', 'e'],
+    '.ogg': ['-a', '-k', '-s', 'e'],
+    '.mp3': ['-I3', '-S', '-L', '-a', '-k', '-s', 'e'],
+    '.m4a': ['-L', '-a', '-k', '-s', 'e'],
+    '.opus': ['-a', '-k', '-s', 'e'],
+    '.wma': ['-L', '-a', '-k', '-s', 'e'],
+    '.wav': ['-I3', '-L', '-a', '-k', '-s', 'e'],
+    '.aiff': ['-I3', '-L', '-a', '-k', '-s', 'e'],
+    '.wv': ['-S', '-a', '-k', '-s', 'e'],
+    '.ape': ['-S', '-a', '-k', '-s', 'e']
+}
 
 def scan(directory):
     for root, subdirs, files in os.walk(directory):
@@ -34,16 +44,20 @@ def scan(directory):
                 album_extension = extensions[0]
                 os.chdir(root)
                 audio_files = glob.glob("*" + album_extension)
-                command = ['loudgain'] + SCAN_ARGS[album_extension] + audio_files
+                command = [loudgain] + SCAN_ARGS[album_extension] + audio_files
                 subprocess.run(command)
 
 if __name__ == "__main__":
     if shutil.which("loudgain") is None:
-        print("Error: loudgain executable not found. Make sure it's in your PATH before running this script")
-        sys.exit(1)
+        script_dir = os.path.dirname(os.path.realpath(__file__))
+        executable_path = os.path.join(script_dir, EXECUTABLE_TITLE)
+        if os.path.isfile(executable_path):
+            loudgain = executable_path
+        else:
+            print("Error: loudgain not found. Make sure it's in your PATH or the same directory as the script")
+            sys.exit(1)
     if (len(sys.argv)) < 2:
-        print("Error: You must pass the path of the root directory to scan as an argument")
-        print("$ python scan.py /path/to/some/directory")
+        print("Error: You must pass the path of the root directory to scan as the first argument")
         sys.exit(1)
     if os.path.isdir(sys.argv[1]) is False:
         print(f"Error: Directory {sys.argv[1]} does not exist")
