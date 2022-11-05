d06c820fcf4c03fbea245852e14a6f9303fbb3f2
Jeff Byrnes <thejeffbyrnes@gmail.com>
2022-11-05 12:03:44 -0400

    Fix condition for preset file search path

--- src/easymode.cpp
+++ src/easymode.cpp
@@ -455,8 +455,8 @@ static void load_preset(const char *preset)
     // Find preset file from name
     if (!path.has_extension()) {
 
-        // Mac/Linux check user directory before system directory
-#ifdef __unix__
+    // Mac/Windows check user directory before system directory
+#ifndef _WIN32
 #ifdef __APPLE__
         join_paths(path, {(const char*) getenv("HOME"), "Library", EXECUTABLE_TITLE, "presets", preset});
 #else
