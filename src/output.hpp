complexlogic/rsgain

commit b779ace835406dd38fe5c091a4600aa6cf0e6c02

Refactored progress bar

src/output.hpp
@@ -30,6 +30,14 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#ifdef __unix__
+#include <unistd.h>
+#include <syslog.h>
+#include <limits.h>
+#include <sys/ioctl.h>
+#endif
+
+
 #include <fmt/core.h>
 
 #define COLOR_GREEN	"[1;32m"
@@ -58,9 +66,34 @@
 extern int quiet;
 extern int disable_progress_bar;
 
-#define output_ok(format, ...) fmt::print(OK_PREFIX format "\n" __VA_OPT__(,) __VA_ARGS__)
-#define output_warn(format, ...) fmt::print(WARN_PREFIX format "\n" __VA_OPT__(,) __VA_ARGS__)
-#define output_error(format, ...) fmt::print(ERROR_PREFIX format "\n" __VA_OPT__(,) __VA_ARGS__)
-#define output_fail(format, ...) fmt::print(FAIL_PREFIX format "\n" __VA_OPT__(,) __VA_ARGS__)
+#define output_ok(format, ...)    if (!quiet) fmt::print(OK_PREFIX format "\n" __VA_OPT__(,) __VA_ARGS__)
+#define output_warn(format, ...)  if (!quiet) fmt::print(WARN_PREFIX format "\n" __VA_OPT__(,) __VA_ARGS__)
+#define output_error(format, ...) if (!quiet) fmt::print(ERROR_PREFIX format "\n" __VA_OPT__(,) __VA_ARGS__)
+#define output_fail(format, ...)  if (!quiet) fmt::print(FAIL_PREFIX format "\n" __VA_OPT__(,) __VA_ARGS__)
 
-void progress_bar(unsigned ctrl, unsigned long x, unsigned long n, unsigned w);
+
+class ProgressBar {
+    private:
+        int c_prev;
+        int w_prev;
+        int pos_prev;
+        int start;
+        int len;
+        char *buffer;
+
+#ifdef _WIN32
+        CONSOLE_SCREEN_BUFFER_INFO info;
+#else
+        struct winsize ws;
+#endif
+        int get_console_width();
+
+
+    public:
+        ProgressBar(void) : buffer(NULL) {};
+        void begin(int start, int len);
+        void update(int pos);
+        void complete(void);
+        void finish(void);
+};
+

src/easymode.cpp
@@ -325,7 +325,6 @@ static void load_overrides(const char *overrides_file)
     }
 }
 
-
 void copy_string_alloc(char **dest, const char *string)
 {
   int length = strlen(string);

src/output.cpp
@@ -65,86 +65,73 @@ extern int multithread;
 static void get_screen_size(unsigned *w, unsigned *h);
 void quit(int status);
 
-void progress_bar(unsigned ctrl, unsigned long x, unsigned long n, unsigned w) {
-	int i;
-	static int show_bar = 0;
-	int c;
-	static int prev_c = -1;
-	static char *buffer = NULL;
-	static double ratio;
-	
-	switch (ctrl) {
-		case 0: /* init */
-			if (quiet || multithread)
-				break;
-#ifndef _WIN32
-			if (!isatty(fileno(stdout)))
-				break;
-#endif
 
-			show_bar = 1;
-			break;
-
-		case 1: /* draw */
-			if (show_bar != 1)
-				return;
-
-			if ((x != n) && (x % (n / 100 + 1) != 0))
-				return;
-
-			if (w == 0) {
-				get_screen_size(&w, NULL);
-				w -= 8;
-				buffer = new char[w + 3];
-			}
-			
-			ratio = x / (double) n;
-			c     = ratio * w;
-			
-			// Only update if the progress bar has incremented
-			if (c != prev_c) {
-				fmt::print(" {:3.0f}% [", ratio * 100);
-				memset(buffer, '=', c);
-				memset(buffer + c, ' ', w - c);
-				buffer[w] = ']';
-				buffer[w + 1] = '\r';
-				buffer[w + 2] = '\0';
-				print_buffer(buffer, w + 2, stdout);
-				prev_c = c;
-			}
-			break;
-
-		case 2: /* end */
-			if (show_bar == 1) {
-				print_buffer("\n", 1, stdout);
-				delete buffer;
-				buffer = NULL;
-			}
-			break;
-	}
+void ProgressBar::begin(int start, int len)
+{
+	this->start = start;
+	this->len = len;
+	w_prev = -1;
+	c_prev = -1;
+	pos_prev = -1;
 }
 
-static void get_screen_size(unsigned *w, unsigned *h) {
-  
-	#ifdef _WIN32
-	CONSOLE_SCREEN_BUFFER_INFO info;
-	GetConsoleScreenBufferInfo(console, &info);
-	if (w != NULL) {
-		*w = info.srWindow.Right - info.srWindow.Left + 1;
-	}
-	if (h != NULL) {
-		*h = info.srWindow.Bottom - info.srWindow.Top + 1;
-	}
-  
-	#else
-	struct winsize ws;
-	if (ioctl(fileno(stdout), TIOCGWINSZ, &ws) < 0 || !ws.ws_row || !ws.ws_col)
+void ProgressBar::update(int pos)
+{
+	int w, c;
+	if (pos == pos_prev)
 		return;
 
-	if (w != NULL)
-		*w = ws.ws_col;
+	w = this->get_console_width() - 8;
+	if (w != w_prev) {
+		delete buffer;
+		buffer = new char[w + 3];
+	}
+	float percent = ((float) pos / (float) len);
+	c = (int) (percent * (float) w);
 
-	if (h != NULL)
-		*h = ws.ws_row;
-	#endif
+	// Only output if we've actually made progess, or the console width changed
+	if (w != w_prev || c != c_prev) {
+		fmt::print(" {:3.0f}% [", percent * 100.f);
+		int i;
+		for (i = 0; i < c; i++)
+			buffer[i] = '=';
+		
+		for (i; i < w; i++)
+			buffer[i] = ' ';
+
+		buffer[w] = ']';
+		buffer[w + 1] = '\r';
+		buffer[w + 2] = '\0';
+		print_buffer(buffer, w + 2, stdout);
+	}
+
+	c_prev = c;
+	w_prev = w;
+	pos_prev = pos;
 }
+
+void ProgressBar::complete()
+{
+	if (c_prev != w_prev)
+		this->update(len);
+}
+
+void ProgressBar::finish()
+{
+	delete buffer;
+	buffer = NULL;
+	fmt::print("\n");
+}
+
+inline int ProgressBar::get_console_width()
+{
+#ifdef _WIN32
+	GetConsoleScreenBufferInfo(console, &info);
+	return info.srWindow.Right - info.srWindow.Left + 1;
+#else
+	if (ioctl(fileno(stdout), TIOCGWINSZ, &ws) < 0 || !ws.ws_col)
+		return 0;
+	return ws.ws_col;
+#endif
+}
+

src/rsgain.cpp
@@ -68,6 +68,7 @@ static inline void help_easy(void);
 
 // Global variables
 int multithread = 0;
+ProgressBar progress_bar;
 
 #ifdef _WIN32
 HANDLE console;
@@ -129,7 +130,7 @@ void parse_pregain(const char *value, Config &config)
 void parse_mode(const char *value, Config &config)
 {
     // for mp3gain compatibilty, include modes that do nothing
-    char *valid_modes = "cdielavsr";
+    const char *valid_modes = "cdielavsr";
     config.mode = value[0];
     if (strchr(valid_modes, config.mode) == NULL)
         output_fail("Invalid tag mode: '{}'", value);

src/scan.cpp
@@ -51,9 +51,9 @@ extern "C" {
 #include "output.hpp"
 #include "tag.hpp"
 
-extern "C" {
-    extern int multithread;
-}
+extern int multithread;
+extern ProgressBar progress_bar;
+
 
 static void scan_av_log(void *avcl, int level, const char *fmt, va_list args);
 
@@ -132,11 +132,12 @@ bool scan(int nb_files, char **files, Config &config)
 
 bool scan_file(const char *file, unsigned index, std::mutex *m) {
     int rc, stream_id = -1;
-    double start = 0, len = 0;
+    int start = 0, len = 0, pos = 0;
     uint8_t *swr_out_data = NULL;
     char infotext[20];
     char infobuf[512];
     bool error = false;
+    bool output_progress = !quiet && !multithread;
     std::unique_lock<std::mutex> *lk = NULL;
     if (m != NULL)
         lk = new std::unique_lock<std::mutex>(*m, std::defer_lock);
@@ -144,11 +145,11 @@ bool scan_file(const char *file, unsigned index, std::mutex *m) {
         output_ok("Scanning '{}' ...", file);
 
     // FFmpeg 5.0 workaround
-    #if LIBAVCODEC_VERSION_MAJOR >= 59 
+#if LIBAVCODEC_VERSION_MAJOR >= 59 
     const AVCodec *codec = NULL;
-    #else
+#else
     AVCodec *codec = NULL;
-    #endif
+#endif
 
     AVPacket packet;
     AVCodecContext *ctx = NULL;
@@ -158,7 +159,6 @@ bool scan_file(const char *file, unsigned index, std::mutex *m) {
     ebur128_state **ebur128 = &scan_states[index];
 
     int buffer_size = 192000 + AV_INPUT_BUFFER_PADDING_SIZE;
-
     uint8_t *buffer = (uint8_t*) malloc(sizeof(uint8_t) * buffer_size);
 
     if (index >= scan_nb_files) {
@@ -187,7 +187,6 @@ bool scan_file(const char *file, unsigned index, std::mutex *m) {
     if (rc < 0) {
         char errbuf[2048];
         av_strerror(rc, errbuf, 2048);
-
         output_fail("Could not find stream info: {}", errbuf);
         error = true;
         goto end;
@@ -232,7 +231,7 @@ bool scan_file(const char *file, unsigned index, std::mutex *m) {
     if (ctx->bits_per_raw_sample > 0 || ctx->bits_per_coded_sample > 0) {
         snprintf(infotext, 
             sizeof(infotext), 
-            "{} bit, ",
+            "%d bit, ",
             ctx->bits_per_raw_sample > 0 ? ctx->bits_per_raw_sample : ctx->bits_per_coded_sample
         );
     }
@@ -298,14 +297,13 @@ bool scan_file(const char *file, unsigned index, std::mutex *m) {
     }
 
     if (container->streams[stream_id]->start_time != AV_NOPTS_VALUE)
-        start = container->streams[stream_id]->start_time *
-                av_q2d(container->streams[stream_id]->time_base);
+        start = container->streams[stream_id]->start_time * av_q2d(container->streams[stream_id]->time_base);
 
     if (container->streams[stream_id]->duration != AV_NOPTS_VALUE)
-        len   = container->streams[stream_id]->duration *
-                av_q2d(container->streams[stream_id]->time_base);
+        len  = container->streams[stream_id]->duration * av_q2d(container->streams[stream_id]->time_base);
 
-    progress_bar(0, 0, 0, 0);
+    if (output_progress)
+        progress_bar.begin(start, len);
     if (lk != NULL)
         lk->unlock();
     
@@ -327,7 +325,7 @@ bool scan_file(const char *file, unsigned index, std::mutex *m) {
                     break;
                 }
                 if (rc >= 0) {
-                    double pos = frame->pkt_dts*av_q2d(container->streams[stream_id]->time_base);
+                    pos = frame->pkt_dts*av_q2d(container->streams[stream_id]->time_base);
 
                     // Convert frame with swresample if necessary
                     if (swr != NULL) {
@@ -342,11 +340,7 @@ bool scan_file(const char *file, unsigned index, std::mutex *m) {
 
                         swr_out_data = (uint8_t*) av_malloc(out_size);
 
-                        if (swr_convert(swr, 
-                        (uint8_t**) &swr_out_data, 
-                        frame->nb_samples,
-                        (const uint8_t**) frame->data, 
-                        frame->nb_samples) < 0) {
+                        if (swr_convert(swr, (uint8_t**) &swr_out_data, frame->nb_samples, (const uint8_t**) frame->data, frame->nb_samples) < 0) {
                             output_fail("Cannot convert");
                             error = true;
                             av_free(swr_out_data);
@@ -367,8 +361,8 @@ bool scan_file(const char *file, unsigned index, std::mutex *m) {
                     if (rc != EBUR128_SUCCESS)
                         output_error("Error filtering");
 
-                    if (pos >= 0)
-                        progress_bar(1, pos - start, len, 0);
+                    if (pos >= 0 && output_progress)
+                        progress_bar.update(pos);
                 }
             }
             av_frame_unref(frame);
@@ -378,10 +372,12 @@ bool scan_file(const char *file, unsigned index, std::mutex *m) {
     }
 
     // complete progress bar for very short files (only cosmetic)
-    progress_bar(1, len, len, 0);
+    if (output_progress)
+        progress_bar.complete();
 
 end:
-    progress_bar(2, 0, 0, 0);
+    if (output_progress)
+        progress_bar.finish();
 
     av_frame_free(&frame);
     

src/tag.cpp
@@ -53,7 +53,6 @@
 #include <mp4file.h>
 #include <opusfile.h>
 #include <asffile.h>
-// #include <rifffile.h>
 #include <wavfile.h>
 #include <aifffile.h>
 #include <wavpackfile.h>
@@ -75,6 +74,7 @@ enum RG_ENUM {
     RG_ALBUM_RANGE,
     RG_REFERENCE_LOUDNESS
 };
+
 
 static const char *RG_STRING_UPPER[] = {
     "REPLAYGAIN_TRACK_GAIN",

commit 381d9104ac01953863d3df9cde27e3138d2bdaa9

Fix building on macOS w/ progress bar changes
Fix issue potentially introduced in b779ace835

Fixes #32

src/output.hpp
@@ -39,6 +39,10 @@
 #include <sys/ioctl.h>
 #endif
 
+#ifdef __APPLE__
+#include <util.h>
+#endif
+
 #ifdef _WIN32
 #include <windows.h>
 #endif