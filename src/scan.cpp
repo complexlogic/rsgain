63f3a54c85fd754c84ebdeb06f91dbe2afbb3a64
Michiel Schriever <scryver@gmail.com>
2025-10-28 15:51:27 +0100

    Moved out the common case in handling of album calculation

--- src/scan.cpp
+++ src/scan.cpp
@@ -671,26 +671,21 @@ void ScanJob::Track::calculate_loudness(const Config &config)
 
 void ScanJob::calculate_album_loudness() 
 {
+    double album_loudness, album_peak;
     if (config.album_as_aes77) {
-        double album_loudness = -HUGE_VAL, album_peak = 0.0, album_gain;
+        album_loudness = -HUGE_VAL;
+        album_peak = 0.0;
         for (const Track &track : tracks) {
             if (album_loudness < track.result.track_loudness) {
                 album_loudness = track.result.track_loudness;
-                album_gain = track.result.track_gain;
             }
             if (album_peak < track.result.track_peak) {
                 album_peak = track.result.track_peak;
             }
         }
-        for (Track &track : tracks) {
-            track.result.album_gain = album_gain;
-            track.result.album_peak = album_peak;
-            track.result.album_loudness = album_loudness;
-        }
     }
 
     else {
-        double album_loudness, album_peak;
         size_t nb_states = tracks.size();
         std::vector<ebur128_state*> states(nb_states);
         for (const Track &track : tracks)
@@ -704,13 +699,13 @@ void ScanJob::calculate_album_loudness()
                          tracks.end(),
                          [](const auto &a, const auto &b) { return a.result.track_peak < b.result.track_peak; }
                      )->result.track_peak;
+    }
 
-        double album_gain = (type == FileType::OPUS && config.opus_mode == 's' ? -23.0 : config.target_loudness)
-                             - album_loudness;
-        for (Track &track : tracks) {
-            track.result.album_gain = album_gain;
-            track.result.album_peak = album_peak;
-            track.result.album_loudness = album_loudness;
-        }
+    double album_gain = (type == FileType::OPUS && config.opus_mode == 's' ? -23.0 : config.target_loudness)
+                         - album_loudness;
+    for (Track &track : tracks) {
+        track.result.album_gain = album_gain;
+        track.result.album_peak = album_peak;
+        track.result.album_loudness = album_loudness;
     }
 }
